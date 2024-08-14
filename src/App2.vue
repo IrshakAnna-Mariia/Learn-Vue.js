7051439bad53cef5f4162efc778e51cf91086720
IrshakAnna-Mariia <irshakam@gmail.com>
2024-08-15 01:35:51 +0300

    feat: create simple form in vue (learn basics)

---
+++ src/App2.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('My first vue application')
+const author = ref('Alex')
+const age = ref(21)
+</script>
+
+<template>
+  <div>{{ title }} - {{ author }} - {{ age }}</div>
+  <br />
+  <button @click="age++">Increase age</button>
+  <button @click="age--">Decrease age</button>
+  <p>Change title here:</p>
+  <input v-model="title" placeholder="Type here" />
+  <p>Change author here:</p>
+  <input v-model="author" placeholder="Type here" />
+</template>
